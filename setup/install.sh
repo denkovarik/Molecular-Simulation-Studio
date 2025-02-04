d3c29a9a7d9f77cbad7277c874af5b99de61d873
Dennis <denkovarik123@gmail.com>
2025-02-03 20:57:54 -0500

    Included steps to install Catch2

--- setup/install.sh
+++ setup/install.sh
@@ -2,6 +2,15 @@
 
 echo "Updating package list..."
 sudo apt update
+sudo apt upgrade -y
 
 echo "Installing build-essentials"
 sudo apt install -y build-essential
+
+# Install Catch2
+cd ~
+git clone -b v2.x https://github.com/catchorg/Catch2.git
+cd Catch2
+mkdir build && cd build
+cmake .. -DBUILD_TESTING=OFF
+sudo make install
