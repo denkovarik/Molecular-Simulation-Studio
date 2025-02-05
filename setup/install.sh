389266ce37ea173ea342c87299f75954e19295c4
Dennis <denkovarik123@gmail.com>
2025-02-04 22:13:30 -0500

    Added dependency

--- setup/install.sh
+++ setup/install.sh
@@ -7,6 +7,8 @@ sudo apt upgrade -y
 echo "Installing build-essentials"
 sudo apt install -y build-essential
 
+sudo apt-get install libgl1-mesa-dev libglfw3 libglfw3-dev
+
 # Install Catch2
 cd ~
 git clone -b v2.x https://github.com/catchorg/Catch2.git
