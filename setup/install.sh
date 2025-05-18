45180e53fb5b92f04a10106b9720c3fda21ce1f1
Dennis <denkovarik123@gmail.com>
2025-05-18 18:22:44 -0400

    Added dependencies

--- setup/install.sh
+++ setup/install.sh
@@ -9,6 +9,13 @@ sudo apt install -y build-essential
 
 sudo apt-get install libgl1-mesa-dev libglfw3 libglfw3-dev
 
+sudo apt install libglm-dev
+
+sudo apt install libxxf86vm-dev libxinerama-dev libxcursor-dev
+
+# OpenGL
+sudo apt-get install freeglut3-dev
+
 # Install Catch2
 cd ~
 git clone -b v2.x https://github.com/catchorg/Catch2.git
